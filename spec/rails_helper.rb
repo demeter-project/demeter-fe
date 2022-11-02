e0d4fc80eeb4b2861309ae7772917034a7523a8e
Thomas Musselman <tmusselman@gmail.com>
2022-11-02 14:49:09 -0600

    remove unnecessary factorybot require

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
 SimpleCov.start
-require 'support/factory_bot'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
