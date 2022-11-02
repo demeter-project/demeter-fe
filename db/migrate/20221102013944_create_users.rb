2dff8dac5cf3e1ee4db99c8a7a424fa5dcb50886
Ken Lenhart <omnipaid@gmail.com>
2022-11-02 09:37:45 -0700

    Refactor oauth, remove password from user model, remove bcrypt gem

--- db/migrate/20221102013944_create_users.rb
+++ db/migrate/20221102013944_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :username
       t.string :email
-      t.string :password_digest
       t.string :uid
       t.string :provider
 
