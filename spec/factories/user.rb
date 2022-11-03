9c583b0c3026b605952b26d80f82a5aac8c70fa4
Ken Lenhart <omnipaid@gmail.com>
2022-11-02 21:20:51 -0700

    Remove password from user.rb factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :user do
     username { Faker::Coffee.blend_name }
     email { Faker::Internet.email }
-    password { Faker::Number.number(digits: 6) }
     uid { Faker::Number.number(digits: 4) }
     provider { "google" }
   end
