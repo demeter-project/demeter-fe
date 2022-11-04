0ab4fcea5ba3f8feb3c9285e4ae761ab460f8dbe
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-04 16:19:33 -0400

    add feature garden#show displays garden information, plots, forecast

---
+++ spec/features/gardens/show_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+require './spec/fixtures/webmock/sample_responses'
+
+RSpec.describe 'garden show page' do
+  include SampleResponses
+
+  before :each do
+    @api_uri = 'https://demeter-be.herokuapp.com'
+    @user = create(:user, id: 1)
+    stub_omniauth(@user)
+    visit landing_page_path
+    click_on "Log in with Google"
+  end
+
+  describe 'when I visit the garden show page' do
+    it 'displays all the plots within that garden' do
+      stub_request(:get, "#{@api_uri}/api/v1/gardens/1")
+        .to_return(body: garden_show.to_json)
+
+      stub_request(:get, "#{@api_uri}/api/v1/gardens/1/plots")
+        .to_return(body: plot_index.to_json)
+
+      visit "/gardens/1?user_id=#{@user.id}"
+      
+      garden = GardenFacade.get_garden(1)
+      plots = GardenFacade.get_garden_plots(garden.id)
+
+      expect(page).to have_content(garden.name)
+
+      plots.each do |plot|
+        within("#plot-#{plot.id}") do
+          expect(page).to have_content(plot.name)
+          # expect(page).to have_content(plot.soil_ph_min)
+          # expect(page).to have_content(plot.soil_ph_max)
+          # expect(page).to have_content(plot.shade_tolerant?)
+          # expect(page).to have_content(plot.contains_toxic?)
+        end
+      end
+
+      expect(page).to have_content(garden.weather_forecast.first.detailed_forecast)
+    end
+  end
+end
