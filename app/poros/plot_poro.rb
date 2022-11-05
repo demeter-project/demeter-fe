41421c2a2e4f0299935481139a248dfaaae754ba
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-05 14:07:57 -0400

    remove garden_id from plot poro - theory is that plot doesnt need to know

--- app/poros/plot_poro.rb
+++ app/poros/plot_poro.rb
@@ -1,19 +1,17 @@
 class PlotPoro
   attr_reader :id,
               :name,
-              :garden_id,
               :soil_ph_min,
-              :soil_ph_max,
-              :garden_id
+              :soil_ph_max
 
-  def initialize(data, garden_id)
+  def initialize(data)
     @id = data[:id]
     @name = data[:attributes][:name]
     @soil_ph_min = data[:attributes][:soil_ph_min]
     @soil_ph_max = data[:attributes][:soil_ph_max]
     @shade_tolerant = data[:attributes][:shade_tolerant?]
     @contains_toxic = data[:attributes][:contains_toxic?]
-    @garden_id = garden_id
+    # @garden_id = garden_id
   end
 
   def shade_tolerant?
