b7a2e619ad56c639d55f467dfcee5ae6a3d778b6
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-05 13:18:25 -0400

    add get_plants method

--- app/facades/plant_facade.rb
+++ app/facades/plant_facade.rb
@@ -1,4 +1,9 @@
 class PlantFacade < DatabaseService
+  def self.get_plants(zip_code: nil, state_code: nil, sort_by: nil, search_name: nil)
+    require 'pry'; binding.pry
+    json = DatabaseService.get_plants_endpoint(plant_id, zip_code, state_code, search_name)
+
+  end
 
   def self.get_plant(plant_id)       #get_plant_endpoint
     json = DatabaseService.get_plant_endpoint(plant_id)
