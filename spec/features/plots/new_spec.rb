e88d36d66d1e345dc1adaf229b6fef84b6da0e0e
Ken Lenhart <omnipaid@gmail.com>
2022-11-05 13:36:57 -0700

    Add plot response with no plots to sample garden response, change add new plot button on plot show page to get method

--- spec/features/plots/new_spec.rb
+++ spec/features/plots/new_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
+require './spec/fixtures/webmock/plot_creation/sample_garden_response'
 
 RSpec.describe 'plot new page' do
+include SampleGardenResponse
+
   before :each do
+    @api_base = 'https://demeter-be.herokuapp.com'
     @user = create(:user, id: 1)
     stub_omniauth(@user)
     visit landing_page_path
@@ -10,7 +14,17 @@ RSpec.describe 'plot new page' do
 
   describe 'when I visit garden show page' do
     it 'and I click new plot button I am taken to form to create new plot' do
-      visit garden_
+      stub_request(:get, "#{@api_base}/api/v1/gardens/1")
+      .to_return(body: garden_response.to_json)
+
+      stub_request(:get, "#{@api_base}/api/v1/gardens/1/plots")
+      .to_return(body: plot_response_no_plots.to_json)
+
+      visit "/gardens/1?user_id=#{@user.id}"
+
+      click_button "Add a new plot"
+
+      expect(current_path).to eq(new_plot_path)
     end
   end
 end
