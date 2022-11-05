ab4e9e699d32b7d7dfa9a6d52f22707901e1f9da
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-05 14:06:41 -0400

    plot poro instantiated only with plot id

--- app/facades/plot_facade.rb
+++ app/facades/plot_facade.rb
@@ -2,13 +2,13 @@ class PlotFacade  < DatabaseService
 
   def self.get_plot(garden_id, plot_id)     #get_plot_endpoint
     json = DatabaseService.get_plot_endpoint(garden_id, plot_id)
-    Plot.new(json[:data][:attributes], json[:data][:id], garden_id)
+    PlotPoro.new(json[:data])
   end
 
-  def self.get_plot_plants(   #get_plot_plants_endpoint
-    json = DatabaseService.get_plot_plants_endpoint(
-    json.each {|plant| PlotPlant(plant)}
-  end
+  # def self.get_plot_plants()   #get_plot_plants_endpoint
+  #   json = DatabaseService.get_plot_plants_endpoint()
+  #   json.each {|plant| PlotPlant(plant)}
+  # end
 
 
 end
