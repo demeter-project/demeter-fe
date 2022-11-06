1a8149bb49efd128fe139dda9abc47ecbba00139
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-06 10:30:55 -0500

    keep get plants

--- app/facades/plant_facade.rb
+++ app/facades/plant_facade.rb
@@ -1,4 +1,8 @@
 class PlantFacade < DatabaseService
+  def self.get_plants(zip_code: nil, state_code: nil, sort_by: nil, search_name: nil)
+    json = DatabaseService.get_plants_endpoint(plant_id, zip_code, state_code, search_name)
+  end
+
   def self.get_plant(plant_id)       #get_plant_endpoint
     json = DatabaseService.get_plant_endpoint(plant_id)
     Plant.new(json[:data])
