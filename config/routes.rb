46167354a6a97bf220c59e683504e987df496a96
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-06 10:41:03 -0500

    rebase;

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ Rails.application.routes.draw do
 
   get '/dashboard', to: 'users#show'
 
-  resources :plants, only: %i[show]
-  
   resources :gardens, only: %i[show new create destroy] do
-    resources :plots, only: %i[new show create destroy] do
-      get 'plants/discover', to: 'plants#index'
-    end
+    resources :plots, only: %i[show new create destroy]
   end
+  
+  resources :plants, only: %i[show]
+
+  get '/gardens/:garden_id/plots/:plot_id/plants/discover', to: 'plants#index'
+  
+  post '/gardens/:garden_id/plots/:plot_id/plot_plants', to: 'plot_plants#create', as: :plot_plants
 
   get '/auth/:provider/callback', to: 'sessions#create'
 
