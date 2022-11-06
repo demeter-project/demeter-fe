2be0731e051e0fa2a7dd55dc21c774eab4beeea9
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-06 12:35:44 -0500

    update fixtures with plot_plant_index

---
+++ spec/fixtures/webmock/plot_plant_index.rb
@@ -0,0 +1,38 @@
+module PlotPlantIndexResponse
+  def plot_plant_index
+    {
+      "data": [
+      {
+      "id": "1",
+      "type": "plot_plant",
+      "attributes": {
+      "quantity": nil,
+      "date_planted": nil,
+      "plant_name": "tufted evening primrose",
+      "plant_id": "692"
+      }
+      },
+      {
+      "id": "2",
+      "type": "plot_plant",
+      "attributes": {
+      "quantity": nil,
+      "date_planted": nil,
+      "plant_name": "whitestem gooseberry",
+      "plant_id": "869"
+      }
+      },
+      {
+      "id": "3",
+      "type": "plot_plant",
+      "attributes": {
+      "quantity": nil,
+      "date_planted": nil,
+      "plant_name": "prickly rose",
+      "plant_id": "876"
+      }
+      }
+      ]
+      }
+  end
+end
--- spec/fixtures/webmock/sample_response.rb
+++ spec/fixtures/webmock/sample_response.rb
@@ -2,10 +2,12 @@ require_relative 'plot_index'
 require_relative 'garden_show'
 require_relative 'plot_show'
 require_relative 'plant_index'
+require_relative 'plot_plant_index'
 
 module SampleResponses
   include GardenShowResponse
   include PlotIndexResponse
   include PlotShowResponse
   include PlantIndexResponse
+  include PlotPlantIndexResponse
 end
