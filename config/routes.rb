35f16bafd7c0deb7a984ec6c3f372e90dfa42457
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-05 20:56:57 -0400

    add routes for plants#index, plot_plants, nest plots inside of gardens

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ Rails.application.routes.draw do
 
   get '/dashboard', to: 'users#show'
 
-  resources :gardens, only: %i[show new create destroy]
-  resources :plots, only: %i[new destroy]
-  resources :plants, only: %i[index]
+  resources :gardens, only: %i[show new create destroy] do
+    resources :plots, only: %i[new destroy]
+  end
+
+  get '/gardens/:garden_id/plots/:plot_id/plants/discover', to: 'plants#index'
+  
+  post '/gardens/:garden_id/plots/:plot_id/plot_plants', to: 'plot_plants#create', as: :plot_plants
 
   get '/auth/:provider/callback', to: 'sessions#create'
 
