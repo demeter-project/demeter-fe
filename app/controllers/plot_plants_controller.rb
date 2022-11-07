0db55e8021a6ed89123455cb0f939992b6765d11
Sid Mann <sjmann2@outlook.com>
2022-11-07 10:44:05 -0600

    Create method for datetime.now and update

--- app/controllers/plot_plants_controller.rb
+++ app/controllers/plot_plants_controller.rb
@@ -8,4 +8,9 @@ class PlotPlantsController < ApplicationController
     plot_plant = PlotPlantFacade.delete_plot_plant(params[:garden_id], params[:plot_id], params[:id])
     redirect_to garden_plot_path(params[:garden_id], params[:plot_id])
   end
+
+  def update
+    @date_time_now = DateTime.now.strftime("%Y-%m-%dT%H:%M")
+    DatabaseService.update_plot_plant(params[:garden_id], params[:plot_id], params[:id], params[:date_planted], params[:quantity])
+  end
 end
