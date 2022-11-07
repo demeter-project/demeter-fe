9e45d56b67374dbabb0a63c4561bd08c50c0a2e1
Sid Mann <sjmann2@outlook.com>
2022-11-07 12:02:03 -0600

    Add webmock fixtures for plot plant update

--- spec/fixtures/webmock/sample_response.rb
+++ spec/fixtures/webmock/sample_response.rb
@@ -6,6 +6,7 @@ require_relative 'plot_plant_index'
 require_relative 'plot_plant_create'
 require_relative 'new_plot_plants'
 require_relative 'plant_search'
+require_relative 'plot_plant_update'
 
 module SampleResponses
   include GardenShowResponse
@@ -16,4 +17,5 @@ module SampleResponses
   include PlotPlantCreateResponse
   include NewPlotPlantsResponse
   include PlantSearchResponse
+  include PlotPlantUpdateResponse
 end
