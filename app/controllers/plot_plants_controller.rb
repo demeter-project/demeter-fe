d3bb42aebb7f950368136eeec0147091d5f92f1d
Sid Mann <sjmann2@outlook.com>
2022-11-07 15:37:25 -0600

    fix bug in form datetime default

--- app/controllers/plot_plants_controller.rb
+++ app/controllers/plot_plants_controller.rb
@@ -10,7 +10,6 @@ class PlotPlantsController < ApplicationController
   end
 
   def update
-    @date_time_now = DateTime.now.strftime("%Y-%m-%dT%H:%M")
     response = PlotPlantFacade.update_plot_plant(params[:garden_id], params[:plot_id], params[:id], params[:date_planted], params[:quantity])
     redirect_to garden_plot_path(params[:garden_id], params[:plot_id])
   end
