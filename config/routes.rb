007663dcec048993f6ecb9706fd063f1d5f92531
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-06 19:42:25 -0500

    routes for plot plants

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,18 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'users#show'
 
   resources :gardens, only: %i[show new create destroy] do
-    resources :plots, only: %i[show new create destroy]
+    resources :plots, only: %i[show new create destroy] do
+      resources :plot_plants, only: %i[index create], as: :plants
+    end
   end
   
   resources :plants, only: %i[show]
 
   get '/gardens/:garden_id/plots/:plot_id/plants/discover', to: 'plants#index', as: :discover_plants
   
-  post '/gardens/:garden_id/plots/:plot_id/plot_plants', to: 'plot_plants#create', as: :plot_plants
+  # post '/gardens/:garden_id/plots/:plot_id/plot_plants', to: 'plot_plants#create', as: :plot_plants
+  # get '/gardens/:garden_id/plots/:plot_id/plot_plants', to: 'plot_plants#index', as: :plot_plants_index
+
 
   get '/auth/:provider/callback', to: 'sessions#create'
 
