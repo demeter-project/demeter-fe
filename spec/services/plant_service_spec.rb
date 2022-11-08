e76acf29419f3a035e0c2450fe6364dd5a27295d
Ken Lenhart <omnipaid@gmail.com>
2022-11-07 17:47:44 -0800

    Complete plant_service vcr test

--- spec/services/plant_service_spec.rb
+++ spec/services/plant_service_spec.rb
@@ -9,7 +9,21 @@ RSpec.describe PlantService do
     it 'get plant endpoint' do
       response = PlantService.get_plant_endpoint(692)
 
-      require 'pry'; binding.pry
+      expect(response).to be_a(Hash)
+      expect(response[:data]).to be_a(Hash)
+    end
+
+    it 'search query get plants endpoint' do
+      search_params = { state_code: "CA", zip_code: "98119", search_name: "primrose" }
+
+      response = PlantService.get_plants_endpoint(search_params)
+
+      expect(response).to be_a(Hash)
+      expect(response[:data]).to be_a(Array)
+      response[:data].each do |plant|
+        expect(plant[:attributes][:common_name]).to include("primrose")
+        expect(plant[:attributes][:states]).to include("CA")
+      end
     end
   end
 end
