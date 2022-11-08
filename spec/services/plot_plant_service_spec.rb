a36e280cc3fae9df458723966815958428e85827
Ken Lenhart <omnipaid@gmail.com>
2022-11-07 18:28:05 -0800

    Complete plot_plant_service vcr test

--- spec/services/plot_plant_service_spec.rb
+++ spec/services/plot_plant_service_spec.rb
@@ -11,7 +11,37 @@ RSpec.describe PlotPlantService do
 
       response = PlotPlantService.create_plot_plant_endpoint(5, 1, plant_ids)
 
-      require 'pry'; binding.pry
+      expect(response).to be_a(Hash)
+      expect(response[:data]).to be_a(Hash)
+    end
+
+    it 'update plot plant' do
+      date_planted = "1999/11/07"
+      quantity = 10
+
+      response = PlotPlantService.update_plot_plant(5, 1, 2, date_planted, quantity)
+
+      expect(response).to be_a(Hash)
+      expect(response[:data]).to be_a(Hash)
+      expect(response[:data][:attributes][:quantity]).to eq(quantity)
+      expect(response[:data][:attributes][:date_planted]).to eq(date_planted)
+    end
+
+    it 'delete plot plant endpoint' do
+      response = PlotPlantService.delete_plot_plant_endpoint(5, 1, 9)
+
+      expect(response.status).to eq(204)
+      expect(response.reason_phrase).to eq("No Content")
+
+      response = PlotPlantService.delete_plot_plant_endpoint(5, 1, 10)
+
+      expect(response.status).to eq(204)
+      expect(response.reason_phrase).to eq("No Content")
+
+      response = PlotPlantService.delete_plot_plant_endpoint(5, 1, 11)
+
+      expect(response.status).to eq(204)
+      expect(response.reason_phrase).to eq("No Content")
     end
   end
 end
