e306f184e4a7708e351a14cfb24ae80c6168ce54
Ken Lenhart <omnipaid@gmail.com>
2022-11-07 18:00:10 -0800

    Complete plant service vcr test

--- spec/services/plot_service_spec.rb
+++ spec/services/plot_service_spec.rb
@@ -9,7 +9,31 @@ RSpec.describe PlotService do
     it 'get plot endpoint' do
       response = PlotService.get_plot_endpoint(5, 1)
 
-      require 'pry'; binding.pry
+      expect(response).to be_a(Hash)
+      expect(response[:data]).to be_a(Hash)
+    end
+
+    it 'get plot plants endpoint' do
+      response = PlotService.get_plot_plants_endpoint(5, 1)
+
+      expect(response).to be_a(Hash)
+      expect(response[:data]).to be_a(Array)
+    end
+
+    it 'create plot for garden endpoint' do
+      plot_params = { name: "FE Test Plot" }
+
+      response = PlotService.create_plot_for_garden_endpoint(1, plot_params)
+
+      expect(response.status).to eq(201)
+      expect(response.reason_phrase).to eq("Created")
+    end
+
+    it 'delete plot from garden endpoint' do
+      response = PlotService.delete_garden_plot_endpoint(5, 5)
+
+      expect(response.status).to eq(204)
+      expect(response.reason_phrase).to eq("No Content")
     end
   end
 end
