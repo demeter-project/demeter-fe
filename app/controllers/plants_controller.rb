b1d7599999c823a13016e5b8006c1e554a690cd0
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-08 10:52:51 -0500

    add sort_order to plant search methods

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -2,17 +2,16 @@ class PlantsController < ApplicationController
   def index
     @garden = GardenFacade.get_garden(params[:garden_id])
     @plot = PlotFacade.get_plot(params[:garden_id], params[:plot_id])
-    @plants = PlantFacade.get_plants(plant_params)
+    @plants = PlantFacade.get_plants(
+      state_code: @garden.state_code, 
+      zip_code: @garden.zip_code,
+      search_name: params[:search_name],
+      sort_by: params[:sort_by],
+      sort_order: params[:sort_order]
+    )
   end
 
   def show
     @plant = PlantFacade.get_plant(params[:id])
   end
-
-  private
-
-  def plant_params
-    params.permit(:zip_code, :state_code, :search_name, :sort_by)
-  end
-
 end
