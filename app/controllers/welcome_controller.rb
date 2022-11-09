af0d9264f69cf573f1a7cc85104ec5f0c247fe45
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-09 09:54:17 -0500

    bootstrap styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,26 @@
 class WelcomeController < ApplicationController
   def index
+    @contributors = [
+      {
+        name: "Thomas Musselman",
+        gh_link: "https://github.com/musselmanth"
+      },
+      {
+        name: "Riley McCullough",
+        gh_link: "https://github.com/Rileybmcc"
+      },
+      {
+        name: "Sid Mann",
+        gh_link: "https://github.com/sjmann2"
+      },
+      {
+        name: "A.J. Krumholz",
+        gh_link: "https://github.com/ajkrumholz"
+      },
+      {
+        name: "Ken Lenhart",
+        gh_link: "https://github.com/penitent0"
+      }
+    ]
   end
 end
